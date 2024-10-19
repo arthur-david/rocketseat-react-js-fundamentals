f3d5c515a28afe25c4ebf7df2cc1895df61d0fdb
Arthur David <arthurdavid000@gmail.com>
2024-10-19 16:26:02 -0300

    feat: implemeting counter function to like button

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,10 +1,19 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from '../Avatar/Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
     }
 
     return (
@@ -28,10 +37,10 @@ export function Comment({ content, onDeleteComment }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
                         Aplaudir
-                        <span>20</span>
+                        <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
