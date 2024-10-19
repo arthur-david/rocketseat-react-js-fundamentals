d4ef29f8453f885c34f891cd4b28304247a538ed
Arthur David <arthurdavid000@gmail.com>
2024-10-19 14:47:06 -0300

    feat: adding form validation

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -22,6 +22,8 @@ export function Post({ author, content, publishedAt }) {
         addSuffix: true,
     });
 
+    const isNewCommentEmpty = newCommentText.length == 0;
+
     function handleCreateNewComment() {
         event.preventDefault();
 
@@ -31,7 +33,12 @@ export function Post({ author, content, publishedAt }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity("")
         setNewCommentText(event.target.value)
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity("Esse campo é obrigatório, mofi!!!")
     }
 
     function deleteComment(commentToDelete) {
@@ -74,10 +81,14 @@ export function Post({ author, content, publishedAt }) {
                     placeholder="Deixe um comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
